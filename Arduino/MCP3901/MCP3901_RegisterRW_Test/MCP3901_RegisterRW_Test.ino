eea7babc09fed1a885d2b91fb2a48040c04440e0
Chris Miles <miles.chris@gmail.com>
2014-08-20 22:14:58 +1000

    Fixes MCP3901 Register Read/Write Test sketch.

--- Arduino/MCP3901/MCP3901_RegisterRW_Test/MCP3901_RegisterRW_Test.ino
+++ Arduino/MCP3901/MCP3901_RegisterRW_Test/MCP3901_RegisterRW_Test.ino
@@ -10,6 +10,7 @@
 
 // SPI
 const int ADC_CS_N = 8;
+const int DAC_CS_N = 7;
 const int ADC_RESET_N = 5;
 const int ADC_DR_N = 4;
 
@@ -17,92 +18,95 @@ const int address_register_write = 0;
 const int address_register_read  = 1;
 
 const int address_register_config2 = (0x0B << 1);
+const int address_register_statuscom = (0x09 << 1);
 
 
 void setup()
 {
   pinMode (ADC_CS_N, OUTPUT);
+  pinMode (DAC_CS_N, OUTPUT);
   pinMode (ADC_RESET_N, OUTPUT);
   pinMode (ADC_DR_N, INPUT);
   
   // initialize SPI:
-  SPI.setClockDivider(SPI_CLOCK_DIV128);
+  //SPI.setClockDivider(SPI_CLOCK_DIV128);
   SPI.setDataMode(SPI_MODE0);
   //SPI.setBitOrder(SPI_MSB_FIRST);
   SPI.begin();
-  
-  MCP3901Setup();
-  
+
   Serial.begin(9600);
+    
+  digitalWrite(ADC_CS_N,HIGH);
+  digitalWrite(DAC_CS_N,HIGH);
+  digitalWrite(ADC_RESET_N,HIGH);
+  
+  MCP3901_Config_Write(0xFF);
 }
 
 void loop()
 {
   int config2_val = 0;
 
-  delay(5000);
-  
   config2_val = MCP3901_Config_Read();
-  Serial.print("config2 value=");Serial.println(config2_val);
-  delay(5000);
   
-  Serial.println("Writing new config2 value.");
-  MCP3901_Config_Write();
-  
+  MCP3901_Config_Write(0x0E);
+
   config2_val = MCP3901_Config_Read();
-  Serial.print("config2 value=");Serial.println(config2_val);
 }
 
-void MCP3901Setup()
+void MCP3901_Config_Write(int writeval)
 {
-  digitalWrite(ADC_RESET_N, HIGH);
-
-}
-
-void MCP3901_Config_Write()
-{
-  SPI.transfer( address_register_config2 | address_register_write );  // address register
-  SPI.transfer(B00001110);  // change the VREF from internal to external
+  int address = (address_register_config2 | address_register_write);
+  digitalWrite(ADC_CS_N, LOW);
   
+  SPI.transfer(address);  //address register
+  SPI.transfer(writeval);  //config register
+
+  Serial.print("Writing to address: ");
+  Serial.println(address,BIN);
+  Serial.println("");
+
+  Serial.print("Writing to config data: ");
+  Serial.println(writeval,BIN);
+  Serial.println("");
+
+  digitalWrite(ADC_CS_N,HIGH);  
 }
 
 int MCP3901_Config_Read()
 {
-  SPI.transfer( address_register_config2 | address_register_read );  // address register
-  int byte_return = SPI.transfer(0xFF);  // push dummy 1's and get byte return
+  digitalWrite(ADC_CS_N,LOW);  
   
-  return byte_return;
+  int value1;
+  int value2;
+  int value3;
+  int readaddress = (address_register_statuscom | address_register_read);
+  
+  
+  
+  SPI.transfer( readaddress );  //address registe
+  value1 = SPI.transfer( 0xFF );  //Push dummy 1's and get byte return
+  value2 = SPI.transfer( 0xFF );  //Push dummy 1's and get byte return
+  value3 = SPI.transfer( 0xFF );  //Push dummy 1's and get byte return 
+
+  Serial.print("Writing to address: ");
+  Serial.println(readaddress,BIN);
+  Serial.println("");
+  
+  Serial.print("Byte1: ");
+  Serial.println(value1,BIN);
+  Serial.println("");
+  
+  Serial.print("Byte2: ");
+  Serial.println(value2,BIN);
+  Serial.println("");  
+  
+  Serial.print("Byte3: ");
+  Serial.println(value3,BIN);
+  Serial.println("");
+  
+  digitalWrite(ADC_CS_N,HIGH); 
+
+  return value1;  
 }
-
-int MCP3901_Data_Read()
-{
-  return 0; // TODO
-}
-
-
-//void MCP4801Write(int value)
-//{
-//  byte wr_enable = 0;
-//  byte ignored = 0;
-//  byte gain = 1;
-//  byte shdn = 1;
-//  
-//  int command = (wr_enable << 15) | (ignored << 14) | (gain << 13) | (shdn << 12)  | (value << 4);
-//  
-//  byte MSB = (command & 0xff00) >> 8;
-//  byte LSB = (command & 0x00ff);
-//
-//  //Serial.print("value=");Serial.println(value);
-//
-//  
-//  // take the CS pin low to select the chip:
-//  digitalWrite(DAC_CS_N, LOW);
-//   
-//  //  send data via SPI:
-//  SPI.transfer(MSB);
-//  SPI.transfer(LSB);
-//  
-//  // take the CS pin high to de-select the chip:
-//  digitalWrite(DAC_CS_N, HIGH);
-//}
 
